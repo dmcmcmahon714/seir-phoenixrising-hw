6d1d9fd9e622a4dd524bd932fd09b6fe86fd20e0
Daniel McMahon <danielmcmahon@Daniels-MacBook-Pro.local>
2019-12-18 15:29:23 -0800

    homework progress

--- wk2_hw/solutions.js
+++ wk2_hw/solutions.js
@@ -1,5 +1,6 @@
 // 1. The argument is the input, the parameter is how the input is represented in the function.
 // 2. A function is defined by a return value and is not necessarily displayed in the console log. The output value of a function cannot come from a console.log().
+//
 function checkPalindrome(str) {
     var re = /[\W_]/g;
     var lowRegStr = str.toLowerCase().replace(re, '');
@@ -8,3 +9,11 @@ function checkPalindrome(str) {
   }
 console.log(checkPalindrome("Radar"));
 console.log(checkPalindrome("Borscht"));
+//Sum Array
+function sumArray(arr) {
+let sum = 0;
+    for (let sum = 0; sum < arr.length; i++); {
+        sum += sumArray[sum]
+      }
+}
+console.log(sumArray([1, 2, 3, 4, 5, 6]));
