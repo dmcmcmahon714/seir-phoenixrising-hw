09c0ee0544d860abfaa3a5d29f24cbe5f641333f
Daniel McMahon <danielmcmahon@Daniels-MacBook-Pro.local>
2019-12-21 09:34:26 -0800

    took it easy

---
+++ wk3_hw/answers.js
@@ -0,0 +1,39 @@
+//1. A light switch that can be either on or off.
+//  datatype: boolean
+//  data structure example: const light = false;
+//2. A user's email address.
+//  datatype: string
+//  data structure example: const email = "name@example.com";
+//3. A spaceship with a hull, laser blasters, tractor beam
+//  datatype: array
+//  data structure example: const spaceship = ["hull", "laser blasters", "tractor beam"];
+//4. A list of student names from our class
+//  datatype: array
+//  data structure example: const classList = ["Chris", "Phi,", "Logan"];
+//5. A list of names from our class, each with location
+//  datatype: object
+//  data structure example:
+//  const classList = {
+//      name: 'Chris'
+//      location: 'New York'   
+// }
+//6. A list of names from our class, each with location
+//  datatype: object
+//  data structure example:
+//  const classList = {
+//     name: 'Chris'
+//     location: 'New York'
+//     faveShow: 'The Office'   
+// }
+// Take it Easy
+//1.
+const rainbowColors = ["Red", "Orange", "Yellow", "Green", "Blue", "Indigo", "Violet"]
+console.log(rainbowColors[4]);
+//2.
+const dan = {
+    food: 'pizza',
+    hobby: 'yoga',
+    town: 'Los Angeles',
+    faveType: 'Boolean',
+};
+console.log(dan.hobby)
