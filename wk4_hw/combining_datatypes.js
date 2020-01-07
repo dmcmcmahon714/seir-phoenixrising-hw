9cdcd8cc5ddf7a17504b71226b6c04248fcd860c
Daniel McMahon <danielmcmahon@Daniels-MacBook-Pro.local>
2020-01-06 18:49:04 -0800

    Combine objects, arrays, and functions.

--- wk4_hw/combining_datatypes.js
+++ wk4_hw/combining_datatypes.js
@@ -7,4 +7,20 @@ const bottle = {
     }
 };
 console.log(bottle.cap.color);
+const receipt = [
+    {name: "chocolate", price: 500}
+];
+console.log(receipt[0].name);
+const apartmentBuilding = [
+    
+    {name: 'Marshall',
+    address: 1629
+},
+{
+    name: 'Walter',
+    address: 1842
+}
+];
+console.log(apartmentBuilding[0].name);
+
 
