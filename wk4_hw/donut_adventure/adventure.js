9550e8ef13d5c02592d4934cc5875ad4d2e9475f
Daniel McMahon <danielmcmahon@Daniels-MacBook-Pro.local>
2020-01-12 15:47:29 -0800

    created our enemy pizza rat

--- wk4_hw/donut_adventure/adventure.js
+++ wk4_hw/donut_adventure/adventure.js
@@ -11,5 +11,33 @@ class Hero {
     talkSass() {
         console.log(this.catchphrases[Math.random() * this.length)]);
     }
+    announceHealth() {
+        console.log(this.health)
+    }
+    fight() {
+        console.log('I\'m ready to rumble!');
+    }
 
 }
+const dougie = new Hero('Dougie');
+class Enemy {
+    constructor (name) {
+        this.name = name;
+        this.health = 100;
+        this.weapons = {
+            pepperoniStars: 5,
+            cheeseGrease: 10
+        }
+        this.catchphrases = ['i\'m youtube famous', 'i\'m more dangerous than an uncovered sewer']
+    }
+    talkSmack() {
+        console.log(this.catchphrases[Math.random() * this.length)]);
+    }
+    announceHealth() {
+        console.log(this.health);
+    }
+    fight () {
+        console.log('i\'m gonna flatten you like a slice of pepperoni');
+    }
+}
+const pizzaRat = new Enemy('Pizza Rat');
