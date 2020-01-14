fd1e564ea997e0d6b3b8e62b6810acfada881018
Daniel McMahon <danielmcmahon@Daniels-MacBook-Pro.local>
2020-01-14 15:14:09 -0800

    donut adventure done

--- wk4_hw/donut_adventure/adventure.js
+++ wk4_hw/donut_adventure/adventure.js
@@ -1,21 +1,34 @@
 class Hero {
     constructor (name) {
-        this.name: = name;
+        this.name = name;
         this.health = 15;
-        this.weapons = {
-            sprinkleSpray: 5,
-            sugarShock: 10
+        this.weapons = [{
+            name: "sprinkle spray",
+            hp: 5
+        },
+        {
+            name: "sugar shock",
+            hp: 10
         }
-        this.catchphrases: ['i\'m fresher than day old pizza', 'you can\'t count my calories!']
+    ];
+        this.catchphrases = ['i\'m fresher than day old pizza', 'you can\'t count my calories!']
     }
     talkSass() {
-        console.log(this.catchphrases[Math.random() * this.length)]);
+        console.log(this.catchphrases[Math.random() * this.length]);
     }
     announceHealth() {
         console.log(this.health)
     }
-    fight() {
-        console.log('I\'m ready to rumble!');
+    randNUmGenerator (arr) {
+        return Math.floor(Math.random() * arr.length)
+    }
+    randomWeapon() {
+        return this.weapons[this.randNUmGenerator(this.weapons)];
+    }
+    fight(enemy) {
+        let weapon = this.randomWeapon();
+        enemy.health -= weapon.hp;
+        console.log(`${this.name} used ${weapon.name}, it hits ${enemy.name} for ${weapon.hp}`);
     }
 
 }
@@ -31,13 +44,24 @@ class Enemy {
         this.catchphrases = ['i\'m youtube famous', 'i\'m more dangerous than an uncovered sewer']
     }
     talkSmack() {
-        console.log(this.catchphrases[Math.random() * this.length)]);
+        console.log(this.catchphrases[Math.random() * this.length]);
     }
     announceHealth() {
         console.log(this.health);
     }
-    fight () {
-        console.log('i\'m gonna flatten you like a slice of pepperoni');
+    fight (enemy) {
+        enemy.health -= this.weapons.pepperoniStars;
+        console.log(`${this.name} used ${Object.keys(this.weapons)[0]}, it hits ${enemy.name} for ${this.weapons.pepperoniStars}`);
     }
 }
 const pizzaRat = new Enemy('Pizza Rat');
+
+dougie.talkSass();
+pizzaRat.talkSmack();
+dougie.announceHealth();
+pizzaRat.announceHealth();
+
+dougie.fight(pizzaRat);
+pizzaRat.fight(dougie);
+dougie.announceHealth();
+pizzaRat.announceHealth();
