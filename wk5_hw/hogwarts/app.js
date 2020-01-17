ffc3210750ecf9d436298cb382688ca2dd4c5240
Daniel McMahon <danielmcmahon@Daniels-MacBook-Pro.local>
2020-01-17 13:18:56 -0800

    i added my first element and it was fun

--- wk5_hw/hogwarts/app.js
+++ wk5_hw/hogwarts/app.js
@@ -1,3 +1,7 @@
-// if (typeof jQuery == 'undefined') {
-//     console.log('oops! I still have to link my jQuery properly');
-// } else {console.log('I did it! I linked jQuery and this js file!')};
+$(() => {
+    const $container = $('#container').attr('container');
+console.log($container);
+    const $h1 = $('<h1>');
+    $('#container').append('<h1>').text('Harry');
+    console.log($h1);
+});
