e856bbd6527024ac2e846732e31116d459929ead
dmcmcmahon714 <57333568+dmcmcmahon714@users.noreply.github.com>
2020-02-12 14:33:01 -0600

    99 bottles

---
+++ wk9_hw/pass_it_around/server.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var app = express();
+var port = 3000;
+
+app.get('/', (req, res) => {
+    res.send(`
+      <h1> 99 Bottles of Beer on the wall </h1>
+      <a href="/98">Take one down, pass it around</a>
+      `)
+  });
+
+  app.get('/:numberOfBottles', (req, res) => {
+    if (parseInt(req.params.numberOfBottles) === 0){
+      res.send(`
+          <a href='/'>No more beer!! Take me to moar beer!</a>
+        `)
+    } else {
+      res.send(`
+        <h1> ${req.params.numberOfBottles} Bottles of Beer on the wall </h1>
+        <a href="${req.params.numberOfBottles - 1}">Take one down, pass it around</a>
+        `)
+    }
+  });
+
+app.listen(port, () => { 
+	console.log('app is running on port: ', port);
+});
