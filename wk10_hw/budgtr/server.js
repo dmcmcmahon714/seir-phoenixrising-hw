68d01b08949c02de50007cc4c3a20091e6a6d5a8
dmcmcmahon714 <57333568+dmcmcmahon714@users.noreply.github.com>
2020-02-19 12:31:41 -0600

    got budget route working

---
+++ wk10_hw/budgtr/server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const budget = require('./models/budget.js');
+const port = 3000;
+
+app.use((request, response, next)=>{
+    console.log('I run for all routes')
+    // continue with your routing
+    next();
+  })
+
+  app.get('/budget', (request, response)=>{
+    response.render('index.ejs', {
+        budget: budget
+    });
+});
+
+app.get('budgets/:index', (request, response)=>{
+    response.render('show.ejs',
+        {
+            budget: budget[request.params.index]
+        }
+    );
+});
+
+
+app.listen(port,() => {
+    console.log('listening on port' , port);
+});
