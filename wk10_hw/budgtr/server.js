b4dc2def03a1ccca4d7bca5f4cb9cea7be6f5863
dmcmcmahon714 <57333568+dmcmcmahon714@users.noreply.github.com>
2020-02-19 14:46:59 -0600

    got new route working

--- wk10_hw/budgtr/server.js
+++ wk10_hw/budgtr/server.js
@@ -3,13 +3,37 @@ const app = express();
 const budget = require('./models/budget.js');
 const port = 3000;
 
+//middleware
+
+app.use((request, response, next)=>{
+    console.log('I run for all routes')
+    // continue with your routing
+    next();
+  })
+
+app.use(express.urlencoded({extended:false}));
 app.use(express.static('public'));
 
-  app.get('/budget', (request, response)=>{
+app.get('/budget', (request, response)=>{
     response.render('index.ejs', {
         budget: budget
     });
 });
+
+app.post('/budget', (req, res)=>{
+    budget.push(req.body);
+    res.redirect('/budget'); //send the user back to /budget
+  });
+
+  app.post('/budget', (request, response) => {
+    console.log("hit create route");
+    console.log('response.body is: ', request.body)
+    response.json(request.body);
+});
+
+app.get('/budget/new', (request, response)=>{
+    response.render('new.ejs');
+  })
 
 app.get('/budget/:indexOfBudgetArray', (request, response)=>{
     response.render('show.ejs',
@@ -18,10 +42,7 @@ app.get('/budget/:indexOfBudgetArray', (request, response)=>{
         }
     );
 });
-
-app.get('/budget/new', (request, response)=>{
-    response.render('new.ejs');
-  })
+  
 
 
 app.listen(port,() => {
