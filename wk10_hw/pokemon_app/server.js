e45fc6b2259d9c2901007b632a2012593a1a1e54
dmcmcmahon714 <57333568+dmcmcmahon714@users.noreply.github.com>
2020-02-18 19:08:04 -0600

    index.ejs view rendered at pokemon route

--- wk10_hw/pokemon_app/server.js
+++ wk10_hw/pokemon_app/server.js
@@ -1,18 +1,29 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-const pokemon = require('./models/pokemon.js');
 
-app.get('/', (req, res) => {
-    console.log("Up and running");
-    res.send('Welcome to the Pokemon App!');
-  });
+const pokemon = require('./models/pokemon.js')
 
-  app.get('/pokemon', (request, response)=>{
-    response.send(pokemon);
-  });
+app.get('/', (request, response)=>{
+    response.send('Welcome to the Pokemon App')
+    });
 
+    app.get('/pokemon', (req, res) => {
+        // capitalizing just the first letter by looping through the pokemon database
+        for (let poke of pokemon) {
+          // split the name into its each individual letter
+          let splitName = poke.name.split('')
+          // capitalize the first letter
+          splitName[0] = splitName[0].toUpperCase()
+          // join the letters back together
+          poke.name = splitName.join('')
+        }
+        // render
+        res.render('index.ejs', {
+          pokemon: pokemon
+        })
+      })
 
-app.listen(3000, ()=>{
+app.listen(port, ()=>{
     console.log('I am listening on port 3000');
 });
