958b4dd1a3882fcf0a2fd77a4532b2315a215949
dmcmcmahon714 <57333568+dmcmcmahon714@users.noreply.github.com>
2020-02-29 14:28:03 -0600

    struggling with index route

--- wk12_hw/captains_log/server.js
+++ wk12_hw/captains_log/server.js
@@ -1,6 +1,24 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+// Load up mongoose npm as mongoose:
+// Load up mongoose npm as mongoose:
+const mongoose = require("mongoose");
+// allows server to receive json data
+app.use(express.urlencoded({ extended: true }));
+
+// Connect mongoose to mongo db:
+mongoose.connect("mongodb://localhost:27017/logsdb", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+mongoose.connection.once("open", () => {
+  console.log("connected to mongo");
+});
+
+const log = require('./models/logs.js');
+const seedData = require('./models/seed_items.js');
+
 
 // NEW
 app.get("/captains_log/new", (req, res) => {
@@ -8,21 +26,35 @@ app.get("/captains_log/new", (req, res) => {
   });
 
   // CREATE
-  app.post("/items/", (req, res) => {
-    item.create(req.body, (error, result) => {
-      // res.send(result);
-      res.redirect("/show.ejs");
-    });
+  app.post("/logs/", (req, res) => {
+  if (req.body.shipIsBroken === "on") {
+    //if checked, req.body.readyToEat is set to 'on'
+    req.body.shipIsBroken = false;
+  } else {
+    //if not checked, req.body.readyToEat is undefined
+    req.body.shipIsBroken = true;
+  }
+  log.create(req.body, (error, result) => {
+    // res.send(result);
+    res.redirect("/logs");
   });
+});
 
   // INDEX
 app.get("/logs/", (req, res) => {
   log.find({}, (error, logs) => {
-    // res.send(fruits);
+    // res.send(logs);
     res.render('index.ejs')
   });
 });
 
+// SHOW ALL
+app.get("/logs/", (req, res) => {
+  log.find({}, (error, logs) => {
+    // res.send(logs);
+    res.render("show.ejs", { logs });
+  });
+});
 
 
 // WEB SERVER //
