09da6cf780e53a3c703c2a6be97fc9e25080b836
dmcmcmahon714 <57333568+dmcmcmahon714@users.noreply.github.com>
2020-05-12 11:23:22 -0500

    Commit 1: Made Rails server

--- wk20_hw/dating_app/dating_app_api/app/controllers/users_controller.rb
+++ wk20_hw/dating_app/dating_app_api/app/controllers/users_controller.rb
@@ -1,51 +1,46 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :update, :destroy]
+  
 
   # GET /users
   def index
-    @users = User.all
-
-    render json: @users
+    render(json: { users: User.all })
   end
 
   # GET /users/1
   def show
-    render json: @user
+    render(json: User.find(params[:id]))
   end
 
   # POST /users
-  def create
-    @user = User.new(user_params)
+def create
+    user = User.new(user_params)
 
-    if @user.save
-      render json: @user, status: :created, location: @user
+    if user.save
+      render(json: { user: user }, status: 201)
     else
-      render json: @user.errors, status: :unprocessable_entity
+      # Unprocessable Entity
+      render(json: { user: user }, status: 422)
     end
   end
 
   # PATCH/PUT /users/1
   def update
-    if @user.update(user_params)
-      render json: @user
-    else
-      render json: @user.errors, status: :unprocessable_entity
-    end
+    user = User.find(params[:id])
+    user.update(user_params)
+    render(json: { user: user })
   end
 
   # DELETE /users/1
   def destroy
-    @user.destroy
+    user = User.destroy(params[:id])
+    render(status: 204)
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
-    def set_user
-      @user = User.find(params[:id])
-    end
 
     # Only allow a trusted parameter "white list" through.
     def user_params
-      params.require(:user).permit(:name, :starsign, :age, :img, :ltl)
+      params.required(:user).permit(:name, :starsign, :age, :img, :ltl)
     end
 end
