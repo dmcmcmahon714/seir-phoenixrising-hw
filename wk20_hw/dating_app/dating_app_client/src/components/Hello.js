32e2cb8d89357ea63d7db9f6c872fa8c3310cf9b
dmcmcmahon714 <57333568+dmcmcmahon714@users.noreply.github.com>
2020-05-12 11:38:11 -0500

    Commit 3:React module and controller

--- wk20_hw/dating_app/dating_app_client/src/components/Hello.js
+++ wk20_hw/dating_app/dating_app_client/src/components/Hello.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
 
 class Daters extends Component {
+
+  getDaters () {
+    fetch('/daters')
+    .then( daters => console.log(daters))
+  }
   render () {
     return (
       <h1>Hello Dating World</h1>
